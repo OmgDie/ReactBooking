992ad23ae6cff8cea13b8b45a9802d38087b66db
OmgD1e <Simplex.ru@mail.ru>
2023-02-28 01:41:09 +0300

    fix routes and AppRouter

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { routes } from '../routes';
+import { publicRoutes, authRoutes } from '../routes';
 import { AUTH_ROUTE } from '../utils/consts';
 
 const AppRouter = () => {
+    const isAuth = false
     return (
         <Routes>
-            {routes.map(({path, Component}) =>
+            {publicRoutes.map(({path, Component}) =>
+                <Route key={path} path={path} element={<Component/>} exact/>
+            )}
+            {isAuth && authRoutes.map(({path, Component}) =>
                 <Route key={path} path={path} element={<Component/>} exact/>
             )}
             <Route path='*' element={<Navigate to={AUTH_ROUTE}/>} />
--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,15 @@ import Auth from "./pages/Auth";
 import Booking from "./pages/Booking";
 import { AUTH_ROUTE, BOOKING_ROUTE } from "./utils/consts";
 
-export const routes = [
+export const publicRoutes = [
     {
         path: AUTH_ROUTE,
         Component: Auth
-    },
+    }
+    
+]
+
+export const authRoutes = [
     {
         path: BOOKING_ROUTE,
         Component: Booking
